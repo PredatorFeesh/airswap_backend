7c1580a3b4e36eb32775e4aea0b7f1b2ed253abd
Jason Azayev <azayevjason@gmail.com>
2020-11-22 14:00:24 -0500

    Basic auth skeleton

--- App.py
+++ App.py
@@ -1,7 +1,54 @@
-import flask
+from flask import Flask, request
+import jwt
+import datetime
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SECRET_AUTH"] = "*&F78gg7878SG787g787&*G8gG**(G^*(&*G8gg78;l[po[[oin9h])23g.[.]"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    request_json = request.get_json()
+
+    email = request_json['email']
+    password = request_json['password']
+
+    if email is None:
+        return {"err_type": "email", "err_msg": "empty"}
+    if password is None:
+        return {"err_type": "password", "err_msg": "empty"}
+
+    # Otherwise now we want to actually verify
+
+    # @TODO: GET ALL USERS AND ACTUALLY VERIFY VIA THE DATABASE HERE
+
+    # @TODO: END CHECKING THE DATABASE
+    # Otherwise, user auth failed!
+    return {"err_type": "auth", "err_msg": "failed"}
+
+@app.route('/register', methods=['POST'])
+def register():
+    request_json = request.get_json()
+
+    email = request_json['email']
+    password = request_json['password']
+    name = request_json['name']
+    
+    if email is None:
+        return {"err_type": "email", "err_msg": "empty"}
+    if password is None:
+        return {"err_type": "password", "err_msg": "empty"}
+    if name is None:
+        return {"err_type": "name", "err_msg": "empty"}
+
+    # The other fields for user as set in the Profile
+
+    # @TODO: VERIFY USER DOESN'T YET EXIST FROM DATABASE
+    # @TODO: ADD USER IF DOESN'T EXIST
+    # Otherwise, fail:
+
+    return {"err_type": "auth", "err_msg": "failed"}
 
 
 @app.route('/', methods=['GET'])
@@ -9,4 +56,4 @@ def home():
     return "Welcome home!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5001)
