274950485ad3d4a76de4a3a487493fb0233c5a87
Jason Azayev <azayevjason@gmail.com>
2020-11-24 16:57:20 -0500

    Auth login and register works in postman

--- App.py
+++ App.py
@@ -1,62 +1,99 @@
-from flask import Flask, request
-import jwt
-import datetime
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import (
+    JWTManager,
+    jwt_required,
+    create_access_token,
+    get_jwt_identity,
+)
+from werkzeug.security import safe_str_cmp
+
+
+import models
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-app.config["SECRET_AUTH"] = "*&F78gg7878SG787g787&*G8gG**(G^*(&*G8gg78;l[po[[oin9h])23g.[.]"
+app.config[
+    "JWT_SECRET_KEY"
+] = "*&F78gg7878SG787g787&*G8gG**(G^*(&*G8gg78;l[po[[oin9h])23g.[.]"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///airswap.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+jwt = JWTManager(app)
 
 db = SQLAlchemy(app)
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    request_json = request.get_json()
-
-    email = request_json['email']
-    password = request_json['password']
-
-    if email is None:
-        return {"err_type": "email", "err_msg": "empty"}
-    if password is None:
-        return {"err_type": "password", "err_msg": "empty"}
-
-    # Otherwise now we want to actually verify
-
-    # @TODO: GET ALL USERS AND ACTUALLY VERIFY VIA THE DATABASE HERE
-
-    # @TODO: END CHECKING THE DATABASE
-    # Otherwise, user auth failed!
-    return {"err_type": "auth", "err_msg": "failed"}
-
-
-@app.route('/register', methods=['POST'])
+@app.route("/register", methods=["POST"])
 def register():
-    request_json = request.get_json()
+    request_json = request.json
 
-    email = request_json['email']
-    password = request_json['password']
-    name = request_json['name']
+    if (
+        request_json is None
+        or request_json["email"] is None
+        or request_json["password"] is None
+        or request_json["name"] is None
+    ):
+        return jsonify({"err_type": "credentials", "err_msg": "empty"})
 
-    if email is None:
-        return {"err_type": "email", "err_msg": "empty"}
-    if password is None:
-        return {"err_type": "password", "err_msg": "empty"}
-    if name is None:
-        return {"err_type": "name", "err_msg": "empty"}
+    email = request_json["email"]
+    password = request_json["password"]
+    name = request_json["name"]
+
+    # Now we need to split first and second.
+    first, second = name.split(" ")
+
+    # Check whether user is in our database
+    user_db = models.User.query.filter_by(email=email).first()
+    if user_db is not None:
+        return jsonify({"err_type": "user", "err_msg": "exists"})
+
+    # Now add the user to the database
+    # @IFTIME: Encrypt password
+    user_db = models.User(email, password, first, second)
+    db.session.add(user_db)
+    db.session.commit()
+
+    access_token = create_access_token(identity={"id": user_db.id})
+
+    return jsonify({"access_token": access_token})
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    request_json = request.json
+
+    if (
+        request_json is None
+        or request_json["email"] is None
+        or request_json["password"] is None
+    ):
+        return jsonify({"err_type": "credentials", "err_msg": "empty"})
+
+    email = request_json["email"]
+    password = request_json["password"]
 
     # The other fields for user as set in the Profile
+    user = models.User.query.filter_by(email=email, password=password)
 
-    # @TODO: VERIFY USER DOESN'T YET EXIST FROM DATABASE
-    # @TODO: ADD USER IF DOESN'T EXIST
-    # Otherwise, fail:
+    if user is None:
+        return jsonify({"err_type": "auth", "err_msg": "bad login"})
 
-    return {"err_type": "auth", "err_msg": "failed"}
+    access_token = create_access_token(identity={"id": user.id})
+
+    return jsonify(access_token=access_token)
 
 
-@app.route('/', methods=['GET'])
+# @TODO REMOVE THIS. Keeping it temporarily for reference
+@app.route("/test", methods=["POST"])
+@jwt_required
+def test():
+    print(get_jwt_identity())
+    return "Secret"
+
+
+@app.route("/", methods=["GET"])
 def home():
     return "Welcome home!"
 
