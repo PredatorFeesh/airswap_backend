9c4b545e2a7fd67bbd3887f80944276bd0cffd39
Natalia <natalia@Roberts-MacBook-Pro.local>
2020-12-15 13:22:43 -0500

    removed old test

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from App import app, db
 from models import User, Listing, City
 import unittest
-
+from flask import jsonify
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -88,6 +88,11 @@ class UserModelCase(unittest.TestCase):
         city = City.query.filter_by(name="Paris").first()
         self.assertEqual(listing.city_id, city.id)
 
+        user.close_listing()
+        self.assertFalse(listing.is_listed)
+        user.open_listing()
+        self.assertTrue(listing.is_listed)
+
     def test_follows(self):
         user1 = User(
             email="testone@email.com",
@@ -129,47 +134,6 @@ class UserModelCase(unittest.TestCase):
             user2.unfollow(city1.name)
             self.assertEqual(user2.cities.count(), 0)
             self.assertEqual(city1.followers.count(), 1)
-    #
-    # def test_view_listings(self):
-    #     user1 = User(
-    #         email="testone@email.com",
-    #         first_name="John",
-    #         last_name="Doe",
-    #         password="test123",
-    #     )
-    #     user1.add_listing("John's address", "London", "default.jpg", "John's listing description", )
-    #
-    #     user2 = User(
-    #         email="testtwo@email.com",
-    #         first_name="Anna",
-    #         last_name="Green",
-    #         password="test123",
-    #     )
-    #     user2.add_listing("Anna's address", "Paris", "default.jpg", "Anna's listing description", )
-    #
-    #     user3 = User(
-    #         email="testthree@email.com",
-    #         first_name="James",
-    #         last_name="Smith",
-    #         password="test123",
-    #     )
-    #     user3.add_listing("James's address", "Paris", "default.jpg", "John's listing description", )
-    #
-    #     city1 = City(name="London")
-    #     city2 = City(name="Paris")
-    #
-    #     db.session.add(user1)
-    #     db.session.add(user2)
-    #     db.session.add(city1)
-    #     db.session.add(city2)
-    #     db.session.commit()
-    #
-    #     with app.app_context():
-    #         user1.follow(city2.name)
-    #         self.assertEqual(user1.get_listings_in_followed_cities().length, 2)
-    #
-    #     # user1.unfollow(city1)
-        # self.assertListEqual(user1.get_listings_in_followed_cities(), [])
 
 
 if __name__ == "__main__":
